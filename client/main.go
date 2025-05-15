a8c84797493cd2655729b71558f64de994e63106
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-05-15 17:40:43 +0530

    feat(client): send multiple requests sequentially

--- client/main.go
+++ client/main.go
@@ -7,22 +7,26 @@ import (
 )
 
 func main() {
-	playerId := "player1"
-
 	url := "http://localhost:8080/join"
-	data := []byte(fmt.Sprintf(`{"playerId":"%s"}`, playerId))
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	// Enough for ~10000 requests to run under 10s
+	// For higher numbers, use go-routines.
+	for i := 1; i < 9010; i++ {
+		playerId := fmt.Sprintf("playerId:%d", i)
+		data := []byte(fmt.Sprintf(`{"playerId":"%s"}`, playerId))
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	
+		if resp.StatusCode == 200 {
+			fmt.Printf("Player %s joined queue, response: %s\n", playerId, resp.Status)
+		} else {
+			fmt.Printf("Player %s failed to join, response: %s\n", playerId, resp.Status)
+		}
 
-	defer resp.Body.Close() // Prevent resource leaks
-
-	if resp.StatusCode == 200 {
-		fmt.Printf("Player %s joined queue, response: %s\n", playerId, resp.Status)
-	} else {
-		fmt.Printf("Player %s failed to join, response: %s\n", playerId, resp.Status)
+		resp.Body.Close()
 	}
 }
