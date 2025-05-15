3e8cbfe0bf6c497f9ed0ca7fca39092c7ba75573
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-05-15 16:08:42 +0530

    chore(client): initial commit with POST request to /hello

---
+++ client/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	url := "http://localhost:8080/hello"
+	data := []byte(`{"playerId":"player1"}`)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	
+	fmt.Println("Joined queue, response:", resp.Status)
+}
