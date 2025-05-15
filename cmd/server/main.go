2bca65adeb3b48d7a77641a6450e8253eecc1f92
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-05-15 16:08:14 +0530

    chore(cmd/server): initial commit with HTTP server and /hello endpoint.

---
+++ cmd/server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type PlayerRequest struct {
+	PlayerId string `json:"playerId"`
+}
+
+func main() {
+	// Handle URL
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
+		fmt.Fprintln(w, "RoyaleQueue matchmaking server")
+	})
+
+	// hello
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST method allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(w, "Only POST method allowed", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		var req PlayerRequest
+		if err := json.Unmarshal(body, &req); err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("Received player ID: %s\n", req.PlayerId)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Player %s added to the queue", req.PlayerId)
+	})
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
