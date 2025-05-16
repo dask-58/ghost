11b537ee31760dbd6ade4fdc697bd020f9dd9295
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-05-16 15:44:41 +0530

    refactor(queue): Concurrency and Efficiency, replaced in memory storage

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,30 +1,55 @@
 package queue
 
+import "sync"
+
 type Player struct {
 	ID string
+	// Other attributes ... (Skill, Time ?)
 }
 
-// slice to store players in memory.
-// needs synchronization for concurrency.
-var queue []Player
+type PlayerQueue struct {
+	mu				sync.RWMutex // read-write mutex | multiple reader, one writer
+	players 		[]Player
+	playerSet 		map[string]struct{} // only to check existence O(1)
+}
 
-func isinQueue(id string) bool {
-	for _, p := range queue {
-		if p.ID == id {
-			return true
-		}
+func __init_queue() *PlayerQueue {
+	return &PlayerQueue{
+		players: make([]Player, 0),
+		playerSet: make(map[string]struct{}),
 	}
-	return false
 }
 
-func JoinQueue(player Player) bool {
-	if !isinQueue(player.ID) {
-		queue = append(queue, player)
-		return true
+func (pq *PlayerQueue) JoinQueue(player Player) bool {
+	pq.mu.Lock() // full lock
+	defer pq.mu.Unlock()
+
+	// Player already in queue
+	if _, exists := pq.playerSet[player.ID]; exists {
+		return false
 	}
-	return false
+
+	pq.players = append(pq.players, player)
+	pq.playerSet[player.ID] = struct{}{}
+	return true
 }
 
-func GetQueue() []Player {
-	return queue
+func (pq *PlayerQueue) GetQueue() []Player {
+	pq.mu.RLock()
+	defer pq.mu.RUnlock()
+
+	_queue := make([]Player, len(pq.players))
+	copy(_queue, pq.players)
+	return _queue
 }
+
+func (pq *PlayerQueue) Size() int {
+	pq.mu.RLock()
+	defer pq.mu.RUnlock()
+	return len(pq.players)
+}
+
+func (pq *PlayerQueue) RemovePlayers(IDS ...string) {
+	// if needed after matchmaking	
+}
+
