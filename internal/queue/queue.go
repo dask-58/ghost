73e9cf7676240fbc5057705f6a7785a6d33ef3b2
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-05-19 19:34:19 +0530

    feat(queue): implement deqeue functionality

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -34,6 +34,37 @@ func (pq *PlayerQueue) JoinQueue(player Player) bool {
 	return true
 }
 
+// removes and returns the first 'count' players from queue
+func (pq *PlayerQueue) DeQueue(count int) []Player {
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
+
+	if count <= 0 {
+		return []Player{}
+	}
+
+	_count := count
+	playersinqueue := len(pq.players)
+
+	if playersinqueue == 0 {
+		return []Player{}
+	}
+
+	if playersinqueue < count {
+		_count = playersinqueue
+	}
+
+	dequeuedPlayers := make([]Player, _count)
+	copy(dequeuedPlayers, pq.players[:_count])
+
+	for _, p := range dequeuedPlayers {
+		delete(pq.playerSet, p.ID)
+	}
+
+	pq.players = pq.players[_count:]
+	return dequeuedPlayers
+}
+
 func (pq *PlayerQueue) GetQueue() []Player {
 	pq.mu.RLock()
 	defer pq.mu.RUnlock()
